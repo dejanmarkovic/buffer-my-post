803aa0e783df422a90264e074bb8925c7347a40d
Dejan Markovic <dejan@dejanmarkovic.com>
2014-11-03 19:04:25 -0500

    first commit

---
+++ uninstall.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dejan Markovic
+ * Date: 02/09/14
+ * Time: 10:48 AM
+ */
+
+
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
+    exit();
+delete_option('bmp_settings');
+delete_option('bmp_opt_admin_url');
+delete_option('bmp_opt_last_update');
+delete_option('bmp_opt_omit_cats');
+delete_option('bmp_opt_omit_cust_cats');
+delete_option('bmp_opt_max_age_limit');
+delete_option('bmp_opt_age_limit');
+delete_option('bmp_opt_excluded_post');
+delete_option('bmp_opt_post_type');
+delete_option('bmp_opt_no_of_post');
+delete_option('bmp_opt_posted_posts');
+delete_option('bmp_opt_add_text');
+delete_option('bmp_opt_add_text_at');
+delete_option('bmp_opt_include_link');
+delete_option('bmp_opt_custom_hashtag_option');
+delete_option('bmp_opt_custom_hashtag_field');
+delete_option('bmp_opt_hashtags');
+delete_option('bmp_opt_url_shortener');
+delete_option('bmp_opt_custom_url_option');
+delete_option('bmp_opt_use_url_shortner');
+delete_option('bmp_opt_use_inline_hashtags');
+delete_option('bmp_opt_hashtag_length');
+delete_option('bmp_opt_custom_url_field');
+delete_option('bmp_opt_bitly_key');
+delete_option('bmp_opt_bitly_user');
+delete_option('bmp_opt_interval');
+delete_option('bmp_settings');
+delete_option('bmp_enable_log');
+delete_option('bmp_opt_add_text');
+delete_option('bmp_opt_add_text_at');
+delete_option('bmp_opt_use_inline_hashtags');
+delete_option('bmp_opt_use_url_shortner');
+delete_option('bmp_opt_access_token');
+
+
+
